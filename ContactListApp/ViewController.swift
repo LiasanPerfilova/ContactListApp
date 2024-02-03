0e5904967950ae4c81a2a2a6f608b9378d408938
LiaisanPerfilova <perliaisan@gmail.com>
2024-02-03 17:31:20 -0500

    Initial Commit

---
+++ ContactListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactListApp
+//
+//  Created by Liaisan on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
